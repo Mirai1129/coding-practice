84fd0a104e1ac4d57bac1f70d179e1cb792be2b2
Mirai1129 <79797958+Mirai1129@users.noreply.github.com>
2022-11-23 15:05:42 +0800

    change files

--- 2. Files/11-21/最大公因數.py
+++
@@ -1,3 +0,0 @@
-import math
-Math1, Math2 = input('').split(',')
-print(math.gcd(int(Math1), int(Math2)))
--- 2. Files/11-21/最小公倍數.py
+++
@@ -1,3 +0,0 @@
-import math
-Math1, Math2 = input('').split(',')
-print(math.lcm(int(Math1), int(Math2)))
